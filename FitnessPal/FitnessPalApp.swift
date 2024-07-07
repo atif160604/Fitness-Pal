9a7b0970bb44e144952fc7af8c9ed84ff02b8d8b
Atif Agbaotwala <atif.agboat@hotmail.com>
2024-07-07 12:33:22 -0400

    Initial Commit

---
+++ FitnessPal/FitnessPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessPalApp.swift
+//  FitnessPal
+//
+//  Created by Atif Agboatwala on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
