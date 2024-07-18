ccfcd8d7002fc194bebeaf2c7d236545c9444b2c
Atif Agbaotwala <atif.agboat@hotmail.com>
2024-07-18 19:49:47 -0400

    Added a section to show the user which food items they selected

--- FitnessPal/Logging/LogMealView.swift
+++ FitnessPal/Logging/LogMealView.swift
@@ -10,20 +10,39 @@ import SwiftUI
 struct LogMealView: View {
     @State private var allFood: [FoodInfo] = [FoodInfo(name: "Oats", calories: 200, carbohydrate: 40, protein: 25, fat: 10)]
     
+    @State private var selectedFood: [FoodInfo] = []
+    
     var proteinOverview: Macro
     var carbohydrateOverview: Macro
     var fatOverview: Macro
+    
     var foodList: MealInfo
     
     var body: some View {
         List {
-            ForEach(allFood, id: \.id) { food in
-                Button {
-                    foodList.food.append(food)
-                    proteinOverview.consumed += food.protein
-                    carbohydrateOverview.consumed += food.carbohydrate
-                    fatOverview.consumed += food.fat
-                } label: {
+            Section("Available items") {
+                ForEach(allFood, id: \.id) { food in
+                    Button {
+                        foodList.food.append(food)
+                        selectedFood.append(food)
+                        proteinOverview.consumed += food.protein
+                        carbohydrateOverview.consumed += food.carbohydrate
+                        fatOverview.consumed += food.fat
+                    } label: {
+                        VStack(alignment: .leading) {
+                            Text("\(food.name)")
+                            HStack {
+                                Text("Carbohydrate: \(food.carbohydrate)")
+                                Text("Protein: \(food.protein)")
+                                Text("Fat: \(food.fat)")
+                            }
+                            .font(.caption)
+                        }
+                    }
+                }
+            }
+            Section("Added items") {
+                ForEach(selectedFood, id: \.id) { food in
                     VStack(alignment: .leading) {
                         Text("\(food.name)")
                         HStack {
--- FitnessPal/Logging/MealOverView.swift
+++ FitnessPal/Logging/MealOverView.swift
@@ -13,47 +13,52 @@ struct MealOverView: View {
     var body: some View {
         ZStack(alignment: .leading) {
             RoundedRectangle(cornerRadius: 20)
-                .frame(width: 350, height: 150)
+                .frame(width: 350, height: 165)
                 .foregroundStyle(Color.white)
-            
-            HStack {
-                ZStack {
-                    Circle()
-                        .stroke(Color.black.opacity(0.1), style: StrokeStyle(lineWidth: 10))
-                        .frame(width: 100, height: 100)
-                        .padding()
-                    
-                    if (meal.totalFat + meal.totalProtein + meal.totalCarbohydrate) > 0 {
-                        // Carbohydrate Circle
+            VStack {
+                Text("\(meal.name)")
+                    .offset(x:50, y:10)
+                
+                HStack {
+                    ZStack {
+                        Text("hello")
                         Circle()
-                            .trim(from: 0, to: meal.carbPercent)
-                            .stroke(Color.red, style: StrokeStyle(lineWidth: 10))
-                            .rotationEffect(.init(degrees: -90))
+                            .stroke(Color.black.opacity(0.1), style: StrokeStyle(lineWidth: 10))
                             .frame(width: 100, height: 100)
                             .padding()
                         
-                        // Protein Circle
-                        Circle()
-                            .trim(from: meal.carbPercent, to: meal.proteinPercent)
-                            .stroke(Color.yellow, style: StrokeStyle(lineWidth: 10))
-                            .rotationEffect(.init(degrees: -90))
-                            .frame(width: 100, height: 100)
-                            .padding()
-                        
-                        // Fat Circle
-                        Circle()
-                            .trim(from: meal.proteinPercent, to: meal.fatPercent)
-                            .stroke(Color.blue, style: StrokeStyle(lineWidth: 10))
-                            .rotationEffect(.init(degrees: -90))
-                            .frame(width: 100, height: 100)
-                            .padding()
+                        if (meal.totalFat + meal.totalProtein + meal.totalCarbohydrate) > 0 {
+                            // Carbohydrate Circle
+                            Circle()
+                                .trim(from: 0, to: meal.carbPercent)
+                                .stroke(Color.red, style: StrokeStyle(lineWidth: 10))
+                                .rotationEffect(.init(degrees: -90))
+                                .frame(width: 100, height: 100)
+                                .padding()
+                            
+                            // Protein Circle
+                            Circle()
+                                .trim(from: meal.carbPercent, to: meal.proteinPercent)
+                                .stroke(Color.yellow, style: StrokeStyle(lineWidth: 10))
+                                .rotationEffect(.init(degrees: -90))
+                                .frame(width: 100, height: 100)
+                                .padding()
+                            
+                            // Fat Circle
+                            Circle()
+                                .trim(from: meal.proteinPercent, to: meal.fatPercent)
+                                .stroke(Color.blue, style: StrokeStyle(lineWidth: 10))
+                                .rotationEffect(.init(degrees: -90))
+                                .frame(width: 100, height: 100)
+                                .padding()
+                        }
                     }
-                }
-                .padding(.horizontal)
-                VStack(alignment: .leading ,spacing: 20) {
-                    Text("Carbs: \(meal.totalCarbohydrate)")
-                    Text("Protein: \(meal.totalProtein)")
-                    Text("Fat: \(meal.totalFat)")
+                    .padding(.horizontal)
+                    VStack(alignment: .leading ,spacing: 20) {
+                        Text("Carbs: \(meal.totalCarbohydrate)")
+                        Text("Protein: \(meal.totalProtein)")
+                        Text("Fat: \(meal.totalFat)")
+                    }
                 }
             }
         }
